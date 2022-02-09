cf4bf1d455dc7aa93b8baf18fda95d3d3c840cca
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-02-09 16:09:19 +0700

    blog - admin - panel - post.php | blockquote + xmp style

--- admin/panel/post.php
+++ admin/panel/post.php
@@ -119,29 +119,43 @@ if (isset($_POST['upload'])) {
             <div class="judul mb-4">
               <label class="form-label"><i class="fas fa-calendar-plus"></i> Judul</label>
               <textarea type="text" class="form-control bg-dark text-white" name="judul"><?= $preview['judul']; ?></textarea>
-              <div class="form-text fst-italic">
-                * Digunakan untuk judul pada postingan
-                <br>
-                * Catatan: Jangan di inputkan dalam bentuk markdown (cukup text nya saja)
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk judul pada postingan
+                    <br>
+                    * Catatan: Jangan di inputkan dalam bentuk markdown (cukup text nya saja)
+                  </p>
+                </blockquote>
               </div>
             </div>
             <div class="tags mb-4">
               <label class="form-label"><i class="fas fa-tags"></i> Tags</label>
               <textarea type="text" class="form-control bg-dark text-white" name="tags"><?= $preview['tags']; ?></textarea>
-              <div class="form-text fst-italic">* Digunakan untuk tag pada postingan</div>
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk tag pada postingan
+                  </p>
+                </blockquote>
+              </div>
             </div>
             <div class="konten mb-4">
               <label class="form-label"><i class="fas fa-book"></i> Konten</label>
               <textarea class="form-control bg-dark text-white" rows="35" placeholder="Something Text here . . ." name="konten"><?= $preview['konten']; ?></textarea>
-              <div class="form-text fst-italic">
-                * Digunakan untuk konten (isi) pada postingan
-                <br>
-                * Note Penulisan untuk gambar (hapus tanda petik dua setelah <): <br>
-                  <"a href="..." target="_blank">
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk konten (isi) pada postingan
                     <br>
-                    <"img src="..." alt="..." class="img-fluid rounded mx-auto d-block" "/>
-                    <br>
-                  <" /a>
+                    * Note Penulisan Tag untuk Gambar: <br>
+                    <xmp class="fw-bold">
+                      <a href="..." target="_blank">
+                        <img src="..." alt="..." class="img-fluid rounded mx-auto d-block"/>
+                      </a>
+                    </xmp>
+                  </p>
+                </blockquote>
               </div>
             </div>
             <div class="gambar mb-4">
@@ -158,29 +172,43 @@ if (isset($_POST['upload'])) {
             <div class="judul mb-4">
               <label class="form-label"><i class="fas fa-calendar-plus"></i> Judul</label>
               <textarea type="text" class="form-control bg-dark text-white" name="judul"></textarea>
-              <div class="form-text fst-italic">
-                * Digunakan untuk judul pada postingan
-                <br>
-                * Catatan: Jangan di inputkan dalam bentuk markdown (cukup text nya saja)
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk judul pada postingan
+                    <br>
+                    * Catatan: Jangan di inputkan dalam bentuk markdown (cukup text nya saja)
+                  </p>
+                </blockquote>
               </div>
             </div>
             <div class="tags mb-4">
               <label class="form-label"><i class="fas fa-tags"></i> Tags</label>
               <textarea type="text" class="form-control bg-dark text-white" name="tags"></textarea>
-              <div class="form-text fst-italic">* Digunakan untuk tag pada postingan</div>
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk tag pada postingan
+                  </p>
+                </blockquote>
+              </div>
             </div>
             <div class="konten mb-4">
               <label class="form-label"><i class="fas fa-book"></i> Konten</label>
               <textarea class="form-control bg-dark text-white" rows="35" placeholder="Something Text here . . ." name="konten"></textarea>
-              <div class="form-text fst-italic">
-                * Digunakan untuk konten (isi) pada postingan
-                <br>
-                * Note Penulisan untuk gambar (hapus tanda petik dua setelah <): <br>
-                  <"a href="..." target="_blank">
+              <div class="form-text">
+                <blockquote>
+                  <p class="fw-bold">
+                    * Digunakan untuk konten (isi) pada postingan
                     <br>
-                    <"img src="..." alt="..." class="img-fluid rounded mx-auto d-block" "/>
-                    <br>
-                  <" /a>
+                    * Note Penulisan Tag untuk Gambar: <br>
+                    <xmp class="fw-bold">
+                      <a href="..." target="_blank">
+                        <img src="..." alt="..." class="img-fluid rounded mx-auto d-block"/>
+                      </a>
+                    </xmp>
+                  </p>
+                </blockquote>
               </div>
             </div>
             <div class="gambar mb-4">
