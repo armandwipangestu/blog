b1ba9888fb7348574f6a2a907a6bbe73effb73a8
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-04-10 21:23:27 +0700

    remove function tag

--- function/constant.php
+++ function/constant.php
@@ -58,37 +58,3 @@ function getFavIcon()
   #return "assets/img/favicon/terminal.svg";
   return "assets/img/favicon/github.png";
 }
-
-function tagImg()
-{
-  return '
-    <a href="..." target="_blank">
-      <img src="..." alt="..." class="img-fluid rounded mx-auto d-block" />
-    </a>
-  ';
-}
-
-function tagTable() {
-  return '
-    <div class="table-responsive rounded">
-      <table class="table table-dark table-striped table-bordered">
-         <thead>
-            <tr>
-               <th scope="col">...</th>
-               <th scope="col">...</th>
-            </tr>
-         </thead>
-         <tbody>
-            <tr>
-               <td>...</td>
-               <td>...</td>
-            </tr>
-            <tr>
-               <td>...</td>
-               <td>...</td>
-            </tr>
-         </tbody>
-      </table>
-    </div>
-  ';
-}
